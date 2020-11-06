705cc62a2509a45c3811a0f50ed53473f0bde4c1
Jacob Milligan <jacobpmilligan@gmail.com>
2020-11-06 17:57:03 +1100

    add '.inl' as supported header file type

--- build.cmd
+++ build.cmd
@@ -1,22 +1,19 @@
 @echo off
 
-set INSTALL_DIR=%1
-set SECOND_ARG=%2
+set SHOULD_CONFIG=%1
+set INSTALL_DIR=.\install\Release
+set BUILD_DIR=.\build\Release
 
-if "%INSTALL_DIR%"=="" (
-    echo ERROR: missing "CMAKE_INSTALL_PREFIX". First argument must be the directory to install the LLVM binaries and include files
+
+if "%SHOULD_CONFIG%" NEQ "" if "%SHOULD_CONFIG%" NEQ "configure" (
+    echo ERROR: invalid argument "%SHOULD_CONFIG%"
     goto :end
 )
 
-if "%SECOND_ARG%" NEQ "" if "%SECOND_ARG%" NEQ "configure" (
-    echo ERROR: invalid argument "%SECOND_ARG%"
-    goto :end
-)
+cmake .\llvm -B "%BUILD_DIR%" -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" -DLLVM_TARGETS_TO_BUILD=X86 -G "Visual Studio 15 2017" -A x64 -Thost=x64 -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_USE_CRT_DEBUG=MDd -DLLVM_USE_CRT_RELEASE=MD -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON
 
-cmake .\llvm -B .\build -DLLVM_ENABLE_PROJECTS="clang" -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" -DLLVM_TARGETS_TO_BUILD=X86 -G "Visual Studio 15 2017" -A x64 -Thost=x64
-
-if "%SECOND_ARG%" NEQ "configure" (
-    cmake --build .\build --target install --config Release --parallel
+if "%SHOULD_CONFIG%" NEQ "configure" (
+    cmake --build "%BUILD_DIR%" --target install --config Release --parallel
 )
 
 :end
